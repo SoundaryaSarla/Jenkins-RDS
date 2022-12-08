d5ea100efb08a0551640f8e15009b1654b91bfe2
SoundaryaSarla <89843642+SoundaryaSarla@users.noreply.github.com>
2022-12-08 12:39:10 +0530

    Create redshift.py

---
+++ redshift.py
@@ -0,0 +1,14 @@
+import psycopg2
+conn = psycopg2.connect(
+    host="redshift-soundarya.cpiazh88ds78.us-east-1.redshift.amazonaws.com",
+    database="dev",
+    user="awsuser",
+    port=5439,
+    password="Admin12345")
+cur = conn.cursor()
+print("connection succussfully established")
+cur.execute("select city from users")
+print(cur.fetchall())
+print(".........")
+print()
+conn.close()
