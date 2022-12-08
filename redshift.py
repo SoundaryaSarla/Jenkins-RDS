b5b9fcd3b33a4f17d3b9db6be51e777241252a15
SoundaryaSarla <89843642+SoundaryaSarla@users.noreply.github.com>
2022-12-08 14:33:09 +0530

    Update redshift.py

--- redshift.py
+++ redshift.py
@@ -8,7 +8,6 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 print("connection succussfully established")
 cur.execute("select city from users")
-cur.execute("DROP TABLE listing")
 print(cur.fetchall())
 print(".........")
 print()
